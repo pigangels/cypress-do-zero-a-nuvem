6a6c04470edf1b9d728effa831d80e21761f5354
pigangels <as.angel.souza@gmail.com>
2025-03-04 18:41:10 -0300

    configuracoes

---
+++ cypress/e2e/CAT-TAT.cy.js
@@ -0,0 +1,12 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
+
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('verifica o título da aplicação', () => {
+    cy.visit('./src/index.html')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+})
